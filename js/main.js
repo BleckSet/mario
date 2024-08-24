dde6df1d9a28aa3c0b51841bc7f6cbf1815f8fdb
BleckSet <108376728+BleckSet@users.noreply.github.com>
2024-08-24 14:29:02 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,75 +1,61 @@
-$(document).ready(() => {
-  $(".burger_inner").on("swipe", event => {
-    if (event.swipestart.coords[0] < event.swipestop.coords[0]) {
-      $(".burger").removeClass("active");
-    }
-  });
-  $(".burger").click(() => {
-    $(".burger_inner").toggleClass("open");
-    $(".burger").toggleClass("active");
-    $("html").toggleClass("hold");
-  });
-
-
-  $(".redmore").click(() => {
-    $(".step_body").toggleClass("slide");
-    $(".redmore").toggleClass("slide");
-  });
-
-  $('a').click(function(event) {
-    event.preventDefault(); 
-    $(".burger_inner").removeClass("open");
-    $(".burger").removeClass("active");
-    $("html").removeClass("hold");
-    let target = $(this.hash);
-    if (target.length) {
-      $('html, body').animate({
-        scrollTop: target.offset().top - 150
-      }, 800);
-    }
-    return false;
-  });
-  
-  $('.slick-slider').slick({
-    arrows: true,
-    infinite: true,
-    speed: 1000,
-    fade:true ,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    prevArrow: $('.l-arr'),
-    nextArrow: $('.r-arr')
-
-  });
-
-
-
-});
-
-
-$(document).ready(function() {
-  $(".accordion-button").on("click", function() {
-    var clickedButton = $(this);
-    var content = clickedButton.siblings(".accordion-content");
-    
-    if (!clickedButton.hasClass('active')) {
-      $(".accordion-button").removeClass('active');
-      $(".accordion-content").slideUp();
-      $(".accordion_inner").css('min-height', '0'); 
-    }
-
-    clickedButton.toggleClass('active');
-    content.slideToggle(function() {
-      if (content.is(':visible')) {
-        var accordionInner = clickedButton.closest('.accordion_inner');
-        accordionInner.css('height', accordionInner.prop('scrollHeight') + 'px');
+$('.slider.firts').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: false,
+  draggable: true,
+  vertical: true,
+  swipe: true,
+  centerMode: true,
+  swipeToSlide: true,
+  touchThreshold: 10,
+  focusOnSelect: true,
+  responsive: [
+    {
+      breakpoint: 769, // При ширине экрана меньше или равной 769px
+      settings: {
+        autoplay: true,
+        autoplaySpeed: 4000,
+        vertical: false, // Отключение вертикального слайдера
+        slidesToShow: 1, // Показываем один слайд
+        centerMode: false,
+        slidesToScroll: 1 // Прокручиваем один слайд
       }
-    });
-  });
+    }
+  ]
+});
+
+$('.slider.second').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: false,
+  draggable: true,
+  vertical: true,
+  centerMode: true,
+  swipe: true,
+  focusOnSelect: true,
+  swipeToSlide: true,
+  touchThreshold: 10,
+  responsive: [
+    {
+      breakpoint: 769, // При ширине экрана меньше или равной 769px
+      settings: {
+        autoplay: true,
+        autoplaySpeed: 5000,
+        centerMode:false,
+        vertical: false, // Отключение вертикального слайдера
+        slidesToShow: 1, // Показываем один слайд
+        slidesToScroll: 1 // Прокручиваем один слайд
+      }
+    }
+  ]
 });
 
 
-
-
-
+$('.slick-slide a').on('click', function(event) {
+  // Проверяем, если родительский элемент не имеет класса `slick-current`
+  if (!$(this).closest('.slick-slide').hasClass('slick-current')) {
+    event.preventDefault(); // Отменяем стандартное действие ссылки
+  }
+});
